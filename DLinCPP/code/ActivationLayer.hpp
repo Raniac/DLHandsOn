fdcf631e9260102b1abfa0ba533889893d89a726
Raniac <leibingye@outlook.com>
2020-02-25 16:23:51 +0800

    Added SigmoidLayer with forward and backward.

--- DLinCPP/code/ActivationLayer.hpp
+++ DLinCPP/code/ActivationLayer.hpp
@@ -11,6 +11,9 @@ namespace DLHandsOn
 	// y = 1.0 / (1.0 + e^(-x))
 	class SigmoidLayer : public Layer {
 	public:
+        // get all weights
+        virtual std::vector<DataObject*> getAllWeights();
+
         // get all grads
         virtual std::vector<DataObject*> getAllGrads();
 
@@ -25,6 +28,11 @@ namespace DLHandsOn
                               std::vector<DataObject*>& output_diffs);
 	};
 
+    std::vector<DataObject*> SigmoidLayer::getAllWeights() {
+        std::vector<DataObject*> all_weights;
+        return all_weights;
+    }
+
     std::vector<DataObject*> SigmoidLayer::getAllGrads() {
         std::vector<DataObject*> all_grads;
         return all_grads;
@@ -35,7 +43,43 @@ namespace DLHandsOn
         // y = 1.0 / (1.0 + e^(-x))
         assert(inputs.size() == outputs.size(), "Invalid input size and output size.");
         assert(inputs.size() > 0, "Invalid input size, less than zero.");
+
+        for (size_t i = 0; i < inputs.size(); i++) {
+            const DataObject& input = *inputs[i];
+            DataObject& output = *outputs[i];
+            const DataType& input_data = input.getData();
+            DataType& output_data = output.getData();
+
+            for (size_t j = 0; j < input_data.size(); j++) {
+                output_data[j] = 1.0f / (1.0f + std::exp(-input_data[j]));
+            }
+        }
     }
 
-    void SigmoidLayer::backward(const std::vector<DataObject*>& inputs, std::vector<DataObject*>& outputs, std::vector<DataObject*>& input_diffs, std::vector<DataObject*>& output_diffs) {}
+    void SigmoidLayer::backward(const std::vector<DataObject*>& inputs, std::vector<DataObject*>& outputs, std::vector<DataObject*>& input_diffs, std::vector<DataObject*>& output_diffs) {
+        // backward
+        // dy/dx = y * (1 - y)
+        // dloss/dx = dloss/dy * dy/dx = dloss/dy * y * (1 - y)
+        assert(inputs.size() == outputs.size(), "Invalid inputs and outputs size.");
+        assert(input_diffs.size() == inputs.size(), "Invalid inputs and diffs size.");
+        assert(output_diffs.size() == outputs.size(), "Invalid diffs and outputs size.");
+        assert(inputs.size() > 0, "Invalid inputs size.");
+
+        for (size_t i = 0; i < input_diffs.size(); i++) {
+            DataObject& input_diff = *input_diffs[i];
+            const DataObject& output_diff = *output_diffs[i];
+            const DataObject& input = *inputs[i];
+            const DataObject& output = *outputs[i];
+
+            const DataType& input_data = input.getData();
+            const DataType& output_data = output.getData();
+            input_diff.fillValue(0.0f);
+            DataType& input_diff_data = input_diff.getData();
+			const DataType& output_diff_data = output_diff.getData();
+
+            for (size_t j = 0; j < input_diff_data.size(); j++) {
+                input_diff_data[j] = output_diff_data[j] * (output_data[j] * (1.0f - output_data[j]));
+            }
+        }
+    }
 } // namespace DLHandsOn
--- DLinCPP/code/main.cpp
+++ DLinCPP/code/main.cpp
@@ -3,6 +3,7 @@
 #include "General.hpp"
 #include "DataObject.hpp"
 #include "DenseLayer.hpp"
+#include "ActivationLayer.hpp"
 #include "Utilities.hpp"
 
 static void printData(std::string data_info, const DLHandsOn::DataObject& data) {
@@ -48,8 +49,40 @@ static void testDenseLayer() {
     printData("Input Diff After Dense Back Propagation:", input_diff);
 }
 
+static void testSigmoidLayer() {
+    const int batch_size = 2;
+    const int input_size = 3;
+    const int output_size = 3;
+
+    DLHandsOn::DataObject input(DLHandsOn::Shape({ batch_size, input_size }));
+    input.fillValue(0.5f);
+    DLHandsOn::DataObject output(DLHandsOn::Shape({ batch_size, output_size }));
+    output.fillValue(0.0f);
+    DLHandsOn::DataObject input_diff(input.getShape());
+    input_diff.fillValue(0.0f);
+    DLHandsOn::DataObject output_diff(output.getShape());
+    output_diff.fillValue(1.0f);
+
+    printData("Input Data Before Dense:", input);
+    printData("Output Data Before Dense:", output);
+    const std::vector<DLHandsOn::DataObject*> inputs = { &input };
+    std::vector<DLHandsOn::DataObject*> outputs = { &output };
+    std::vector<DLHandsOn::DataObject*> input_diffs = { &input_diff };
+    std::vector<DLHandsOn::DataObject*> output_diffs = { &output_diff };
+
+    DLHandsOn::SigmoidLayer sigmoidLayer;
+    sigmoidLayer.setPhase(DLHandsOn::Layer::Phase::Train);
+
+    sigmoidLayer.forward(inputs, outputs);
+    printData("Input Data After Dense:", input);
+    printData("Output Data After Dense:", output);
+
+    sigmoidLayer.backward(inputs, outputs, input_diffs, output_diffs);
+    printData("Input Diff After Dense Back Propagation:", input_diff);
+}
+
 int main() {
     std::cout << "Deep Learning Hands-On!" << std::endl;
-    testDenseLayer();
+    testSigmoidLayer();
     return 0;
 }
