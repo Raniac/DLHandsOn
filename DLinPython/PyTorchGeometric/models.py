89aa6dc7b2f6950f63cc1ea09e048097d9a27eba
Raniac <leibingye@outlook.com>
2020-02-26 23:07:58 +0800

    Tried differential pooling.

--- DLinPython/PyTorchGeometric/models.py
+++ DLinPython/PyTorchGeometric/models.py
@@ -5,6 +5,7 @@ import torch_geometric.transforms as T
 from torch.nn import Sequential as Seq, Linear as Lin, ReLU
 from torch_geometric.nn import GCNConv, GATConv, GINConv
 from torch_geometric.nn import global_mean_pool, global_max_pool
+from torch_geometric.nn.dense import diff_pool
 
 from torch_geometric.utils import add_self_loops
 
@@ -63,14 +64,13 @@ class GATNet(torch.nn.Module):
         return F.log_softmax(x, dim=1)
 
 ## Graph Isomorphism Network
-"""
-GIN achieves maximal discriminative power by using injective neighbor aggregation.
-"""
+## GIN achieves maximal discriminative power by using injective neighbor aggregation.
 class GIN(torch.nn.Module):
     def __init__(self):
         super(GIN, self).__init__()
         nn = Seq(Lin(4, 16), ReLU(), Lin(16, 4))
         self.conv = GINConv(nn, train_eps=True)
+        self.feat_after_gap = []
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
@@ -79,5 +79,16 @@ class GIN(torch.nn.Module):
         x = F.relu(x)
         # x = F.dropout(x, p=0.5, training=self.training)
         x = global_mean_pool(x, batch)
+        self.feat_after_gap.extend(x)
 
         return F.log_softmax(x, dim=1)
+
+## Hierarchical Graph Representation Learning with Differentiable Pooling
+## https://arxiv.org/abs/1806.08804
+# TODO: study on GDP inputs parameters
+class GDP(torch.nn.Module):
+    def __init__(self):
+        super(GDP, self).__init__()
+
+    def forward(self, data):
+        pass
--- DLinPython/README.md
+++ DLinPython/README.md
@@ -5,7 +5,7 @@
 ### 1.1 Using Docker
 
 ```bash
-docker run -it --rm -v /c/Users/Benny/Documents/Projects/DLHandsOn/DLinPython/pytorch_geometric:/pytorch_geometric imcomking/pytorch_geometric:latest /bin/bash
+docker run -it --rm -v /c/Users/Benny/Documents/Projects/DLHandsOn/DLinPython/PyTorchGeometric:/workspace imcomking/pytorch_geometric:latest /bin/bash
 ```
 
 ### 1.2 Graph Attention Network (GATConv, see appendix for doc)
