ed7f2739b401ef87033458a978d14d9eef657dd8
Raniac <leibingye@outlook.com>
2020-03-11 22:16:32 +0800

    Updated main.

--- DLinPython/TIANCHI/FreshCompOffline/main.py
+++ DLinPython/TIANCHI/FreshCompOffline/main.py
@@ -50,28 +50,61 @@ def train_behavior_prediction(data_path, model_name, model_path):
     :type model_name: str
     :type model_path: str
     """
-    pass
+    
+    data = pd.read_csv(data_path)
+    features = data[['num_views', 'num_favorites', 'num_add2carts']]
+    label = data['buyOrNot']
+    print(features)
+    print(label)
+
+    features = preprocessing.scale(features)
+
+    train_X, test_X, train_y, test_y = train_test_split(features, label, random_state=0)
+
+    model = SVC(C=1, kernel='linear')
+
+    model.fit(train_X, train_y)
+    predictions = model.predict(test_X)
+
+    tn, fp, fn, tp = confusion_matrix(test_y, predictions).ravel()
+    print('The confusion matrix is:', (tn, fp, fn, tp))
+    cnf_accuracy = (tn + tp) / (tn + fp + fn + tp)
+    print('The accuracy is: %.2f' % cnf_accuracy)
+    cnf_sensitivity = tp / (tp + fn)
+    print('The sensitivity is: %.2f' % cnf_sensitivity)
+    cnf_specificity = tn / (tn + fp)
+    print('The specificity is: %.2f' % cnf_specificity)
+
+    joblib.dump(model, model_path)
 
 def predict_behavior(data_path, model_path):
     """
     :type data_path: str
     :type model_path: str
     """
-    pass
+    
+    data = pd.read_csv(data_path)
+    features = data[['num_views', 'num_favorites', 'num_add2carts']]
+    label = data['buyOrNot']
+    features = preprocessing.scale(features)
 
-def item_rank(data_path, model_name, model_path):
+    model = joblib.load(model_path)
+
+    predictions = model.predict(features)
+    # TODO output predictions to a csv file
+
+def item_rank(data_path):
     """
     :type data_path: str
-    :type model_name: str
-    :type model_path: str
     """
-    pass
+    data = pd.read_csv(data_path)
+    # TODO rank items according to their ratings
 
-def predict_item(data_path, model_path):
+def predict_item(data_path):
     """
     :type data_path: str
-    :type model_path: str
     """
+    # TODO predict item using the ranking of its category
     pass
 
 if __name__ == "__main__":
