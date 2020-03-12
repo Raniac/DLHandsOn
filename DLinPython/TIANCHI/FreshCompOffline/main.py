1470832b5f80703673b84dc8d1a6594a89dee900
Raniac <leibingye@outlook.com>
2020-03-12 22:33:41 +0800

    Added predict probability.

--- DLinPython/TIANCHI/FreshCompOffline/main.py
+++ DLinPython/TIANCHI/FreshCompOffline/main.py
@@ -16,7 +16,7 @@ def test_train():
 
     train_X, test_X, train_y, test_y = train_test_split(features, label, random_state=0)
 
-    model = SVC(C=1, kernel='linear')
+    model = SVC(C=1, kernel='linear', probability=True)
 
     model.fit(train_X, train_y)
     predictions = model.predict(test_X)
@@ -41,8 +41,15 @@ def test_predict():
 
     model = joblib.load('models/test.model')
 
-    predictions = model.predict(features)
+    predictions = model.predict_proba(features)
     print(list(predictions)[:10])
+    predictions_df = pd.DataFrame({
+        'user_id': data['user_id'],
+        'item_category': data['item_category'],
+        'predictions': predictions[:, 1] > 0.5,
+        'predict_prob': predictions[:, 1]
+    })
+    predictions_df.to_csv('data/test_predictions.csv', index=False)
 
 def train_behavior_prediction(data_path, model_name, model_path):
     """
@@ -61,7 +68,7 @@ def train_behavior_prediction(data_path, model_name, model_path):
 
     train_X, test_X, train_y, test_y = train_test_split(features, label, random_state=0)
 
-    model = SVC(C=1, kernel='linear')
+    model = SVC(C=1, kernel='linear', probability=True)
 
     model.fit(train_X, train_y)
     predictions = model.predict(test_X)
@@ -77,10 +84,11 @@ def train_behavior_prediction(data_path, model_name, model_path):
 
     joblib.dump(model, model_path)
 
-def predict_behavior(data_path, model_path):
+def predict_behavior(data_path, model_path, predict_path):
     """
     :type data_path: str
     :type model_path: str
+    :type predict_path: str
     """
     
     data = pd.read_csv(data_path)
@@ -90,8 +98,15 @@ def predict_behavior(data_path, model_path):
 
     model = joblib.load(model_path)
 
-    predictions = model.predict(features)
+    predictions = model.predict_proba(features)
     # TODO output predictions to a csv file
+    predictions_df = pd.DataFrame({
+        'user_id': data['user_id'],
+        'item_category': data['item_category'],
+        'predictions': predictions[:, 1] > 0.5,
+        'predict_prob': predictions[:, 1]
+    })
+    predictions_df.to_csv(predict_path, index=False)
 
 def item_rank(data_path):
     """
@@ -100,8 +115,8 @@ def item_rank(data_path):
     data = pd.read_csv(data_path)
     # TODO rank items according to their ratings
     # DataFrame.apply: https://blog.csdn.net/Evan_Blog/article/details/82787984
-    # data = data.groupby(['item_category'], sort=False).apply(lambda x: x.sort_values('rating', ascending=False)).reset_index(drop=True)
-    data = data.sort_values(['item_category', 'rating'], ascending=False)
+    data = data.groupby(['item_category'], sort=False).apply(lambda x: x.sort_values('rating', ascending=False)).reset_index(drop=True)
+    # data = data.sort_values(['item_category', 'rating'], ascending=False)
     print(data)
 
 def predict_item(data_path):
@@ -113,5 +128,5 @@ def predict_item(data_path):
 
 if __name__ == "__main__":
     # test_train()
-    # test_predict()
-    item_rank('data/features/features_item_rating.csv')
+    test_predict()
+    # item_rank('data/features/features_item_rating.csv')
